d7a3cdea65ef321d53b8dc8417175781b5272049
dnaq <dnaq@users.noreply.github.com>
2024-07-28 13:20:40 +0200

    Fix panic when starting helix tutor
    Closes #11351
    
    Also fixed some minor issues related to log
    message contents, and removed unnecessary use
    of `.as_mut()` as per code review comments on
    the PR.

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -1080,22 +1080,25 @@ impl Document {
     }
 
     pub fn pickup_last_saved_time(&mut self) {
-        self.last_saved_time = match self.path.as_mut().unwrap().metadata() {
-            Ok(metadata) => match metadata.modified() {
-                Ok(mtime) => mtime,
-                Err(_) => {
+        self.last_saved_time = match self.path() {
+            Some(path) => match path.metadata() {
+                Ok(metadata) => match metadata.modified() {
+                    Ok(mtime) => mtime,
+                    Err(e) => {
+                        log::error!(
+                            "Using system time instead of fs' mtime: not supported on this platform: {e}"
+                        );
+                        SystemTime::now()
+                    }
+                },
+                Err(e) => {
                     log::error!(
-                        "Use a system time instead of fs' mtime not supported on this platform"
+                        "Using system time instead of fs' mtime: failed to read file's metadata: {e}"
                     );
                     SystemTime::now()
                 }
             },
-            Err(e) => {
-                log::error!(
-                    "Use a system time instead of fs' mtime: failed to file's metadata: {e}"
-                );
-                SystemTime::now()
-            }
+            None => SystemTime::now(),
         };
     }
 
